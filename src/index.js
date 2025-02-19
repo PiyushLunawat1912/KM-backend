825ef9ef9571177b4b7109b5b726d46e50e4990e
PiyushLunawat1912 <piyushpl9527@gmail.com>
2025-02-20 00:05:38 +0530

    server changes updated

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ import { Router } from 'express'; // Router for handling routes
 dotenv.config();
 
 const app = express();
-const HOST = '0.0.0.0'; // Allows binding to all IPs
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
 
@@ -225,6 +225,6 @@ app.all('*', (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, HOST, () => {
-  console.log(`Server running on http://${HOST}:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
